1d8cda7c5bb87a08ac6dd8f6e3cf6c0e16b8622d
EdsonJohn <edson.john.domingo@ibm.com>
2024-12-14 22:31:56 +0900

    add webconfig

--- src/main/kotlin/com/nexxutech/fileuploader/config/WebConfig.kt
+++ src/main/kotlin/com/nexxutech/fileuploader/config/WebConfig.kt
@@ -1,13 +1,13 @@
-//package com.nexxutech.fileuploader.config
-//
-//import org.springframework.context.annotation.Configuration
-//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
-//
-//@Configuration
-//class WebConfig : WebMvcConfigurer {
-//    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
-//        registry.addResourceHandler("/uploads/**")
-//            .addResourceLocations("file:/var/www/uploads/")
-//    }
-//}
+package com.nexxutech.fileuploader.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry.addResourceHandler("/uploads/**")
+            .addResourceLocations("file:/var/www/uploads/")
+    }
+}
