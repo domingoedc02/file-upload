948024792b8a460d98ebb879512c93e0c7e34f03
EdsonJohn <edson.john.domingo@ibm.com>
2024-12-14 15:41:29 +0900

    securityConfig added

---
+++ src/main/kotlin/com/nexxutech/fileuploader/config/SecurityConfig.kt
@@ -0,0 +1,40 @@
+package com.nexxutech.fileuploader.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
+
+    @Primary
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): HttpSecurity {
+        http
+            .cors { it.disable() }
+            .csrf { it.disable() }
+//            .authorizeRequests {
+//                it.anyRequest().permitAll()  // Allow all API requests without authentication
+//            }
+        return http
+    }
+
+    @Bean
+    fun authenticationManager(config: AuthenticationConfiguration): AuthenticationManager {
+        return config.authenticationManager
+    }
+
+
+}
+
